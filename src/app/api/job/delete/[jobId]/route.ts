0a82dca21eef230d59df1884d0d6935194f88e9f
soorajkv <kvsooraj8089@gmail.com>
2025-09-06 11:20:56 +0530

    Completed the delete job route

---
+++ src/app/api/job/delete/[jobId]/route.ts
@@ -0,0 +1,43 @@
+import { authenticate } from "@/lib/auth";
+import { connectDB } from "@/lib/mongodb";
+import { Job } from "@/models/Job";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { jobId: string } }
+) {
+  try {
+    await connectDB();
+    const user = authenticate(req as any);
+    if (!user) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 400 });
+    }
+
+    const { jobId } = params;
+
+    const job = await Job.findById(jobId);
+    if (!job) {
+      return NextResponse.json({ error: "Job not found" }, { status: 404 });
+    }
+
+    if (job.createdBy.toString() !== user.id) {
+      return NextResponse.json(
+        { error: "You are not allowed to delete this job" },
+        { status: 403 }
+      );
+    }
+
+    await Job.findByIdAndDelete(jobId);
+
+    return NextResponse.json(
+      { message: "Deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
