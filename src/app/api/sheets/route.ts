ebe529eef037e775849b0781fc4f5a2c34856650
The RD Group Of Industries <therdgroup24@gmail.com>
2025-09-20 13:43:08 +0530

    Update route.ts

--- src/app/api/sheets/route.ts
+++ src/app/api/sheets/route.ts
@@ -1,5 +1,5 @@
 import { connectDB } from "@/lib/mongodb";
-import Sheet from "@/models/Sheet";
+import { Sheet } from "@/models/Sheet";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
