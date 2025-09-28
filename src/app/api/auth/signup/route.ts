cbe388c8de4c623e761a68d601641119e4e7f349
The RD Group Of Industries <therdgroup24@gmail.com>
2025-09-28 21:02:27 +0530

    Update route.ts

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: NextRequest) {
         name,
         email,
         password,
-        role: role || "user",
+        role: "user",
         phone,
         otp: OTP,
       });
