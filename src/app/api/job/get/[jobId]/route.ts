c48824b503191f2412039a5114e91b65c9e3ff1b
The RD Group Of Industries <therdgroup24@gmail.com>
2025-09-29 00:01:01 +0530

    for app

--- src/app/api/job/get/[jobId]/route.ts
+++ src/app/api/job/get/[jobId]/route.ts
@@ -1,21 +1,28 @@
-import { authenticate } from "@/lib/auth";
 import { connectDB } from "@/lib/mongodb";
 import { Job } from "@/models/Job";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest,
-  { params }: { params: { jobId: string }) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { jobId: string } }
+) {
   try {
     await connectDB();
-    const 
 
-    const jobs = await Job.findById(jobId);
+    const { jobId } = params;
 
-    return NextResponse.json({
-      jobs,
-    });
+    const job = await Job.findById(jobId);
+
+    if (!job) {
+      return NextResponse.json(
+        { error: "Job not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({ job });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return NextResponse.json(
       { error: (error as Error).message },
       { status: 500 }
