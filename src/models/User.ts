d2dd7535bd1bbde2aa6726e0c50cc8a290309003
The RD Group Of Industries <therdgroup24@gmail.com>
2025-10-06 12:59:37 +0530

    Resume...

--- src/models/User.ts
+++ src/models/User.ts
@@ -19,6 +19,7 @@ const UserSchema = new Schema<IUser>(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    resume: { type: String, required: false },
     phone: { type: String, required: false },
     role: {
       type: String,
